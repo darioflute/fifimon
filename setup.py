796b5f91843a10b055e4edcf0f97f3b3cacdc794
darioflute <darioflute@gmail.com>
2017-07-13 15:58:39 -0700

    added files for conda package

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fifimon',
+      version='0.1.alpha',
+      description='FIFI-LS Monitor',
+      long_description='The package monitors FIFI-LS data',
+      author='Dario Fadda',
+      author_email='darioflute@gmail.com',
+      url='https://github.com/darioflute/fifimon.git',
+      download_url='https://github.com/darioflute/fifimon',
+      license='GPLv3+',
+      packages=['fifimon'],
+      scripts=['bin/fifimon']
+     )
