99d69108ba64f88cba7657f3b458b52b2e3b87ca
Fadda <dfadda@arsla17070011.ndc.nasa.gov>
2017-12-19 11:18:29 -0800

    still problems on forking with mac os-x

--- fifimon/start.py
+++ fifimon/start.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from fifimon import mainwindow
-import multiprocessing as mp
 
 def main():
     mainwindow.main()
 
 if __name__ == "__main__":
-    mp.set_start_method('forkserver')
     main()    
